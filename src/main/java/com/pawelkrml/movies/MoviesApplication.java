28b7fc036a09a67a6e4f802646b11a9f294a1994
Paweł Kromołowski <cater981pl@gmail.com>
2024-11-07 15:20:04 +0100

    spring & postgres setup

--- src/main/java/com/pawelkrml/movies/MoviesApplication.java
+++ src/main/java/com/pawelkrml/movies/MoviesApplication.java
@@ -3,10 +3,19 @@ package com.pawelkrml.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSOWRD", dotenv.get("DB_PASSOWRD"));
+		System.setProperty("SPRING_PROFILE", dotenv.get("SPRING_PROFILE"));
+
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
