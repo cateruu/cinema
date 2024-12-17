1fb8dcb8b2ae9c4f95b9c4a588a72d18a03a57fd
Paweł Kromołowski <cater981pl@gmail.com>
2024-12-17 15:04:07 +0100

    init next.js app

---
+++ frontend/src/app/(base)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className='font-[family-name:var(--font-poppins)]'>ehe</div>;
+};
+
+export default Home;
---
+++ frontend/src/app/_components/SideBar/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <aside>
+      <h1 className='font-[family-name:var(--font-krona-one)]'>CINEMA MNGMT</h1>
+    </aside>
+  );
+};
+
+export default SideBar;
